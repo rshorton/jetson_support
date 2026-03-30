35be11540469f13d31991783a22dacb781b59486
Scott Horton <horton.rscott@gmail.com>
2026-03-30 17:11:21 -0500

    Fixed base directory reference

--- run_stt_tts.py
+++ run_stt_tts.py
@@ -4,12 +4,12 @@ echo "Starting STT/TTS processes"
 
 echo "STT..."
 cd ${WS_DIR}/src/elsabot_speech_input/elsabot_speech_input
-python -m server.main 2>&1 | tee ${WS_DIR}/jetson/sst.log &
+python -m server.main 2>&1 | tee ${WS_DIR}/jetson_support/sst.log &
 
 echo "TTS..."
 
-cd ${WS_DIR}/jetson
-python3 -m piper.http_server -m en_US-amy-medium --cuda 2>&1 | tee ${WS_DIR}/jetson/tts.log &
+cd ${WS_DIR}/jetson_support
+python3 -m piper.http_server -m en_US-amy-medium --cuda 2>&1 | tee ${WS_DIR}/jetson_support/tts.log &
 
 sleep 20
 echo "Cntrl-c to exit"

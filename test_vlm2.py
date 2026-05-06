198ac6417362c801abcbdf184b9d1cf0e3be6974
Scott Horton <horton.rscott@gmail.com>
2026-05-05 21:07:36 -0500

    Revised to specify gemma4 and tweak test prompts

--- test_vlm2.py
+++ test_vlm2.py
@@ -9,7 +9,7 @@ def encode_image(image_path):
 
 client = OpenAI(
     api_key="EMPTY",
-    base_url="http://localhost:8001/v1",
+    base_url="http://localhost:8000/v1",
     timeout=3600
 )
 
@@ -25,8 +25,8 @@ prompt2 = 'Please caption the notable attributes in the provided image. List and
 prompt3 = f'Locate the bounding box of {object_name}. Return a json of this format {{"bb_2d": [x, y], "label": object label}}.'
 prompt4 = 'What can be the next immediate action?'
 prompt5 = 'You are given the task "{Place object on the desk}". Specify the 2D trajectory your end effector should follow in pixel space. Return the trajectory coordinates in JSON format like this: {"point_2d": [x, y], "label": "gripper trajectory"}.'
-prompt6 = 'Please caption the notable attributes in the provided image. List and describe all marked subjects in the image with their categories, detailed captions, and bounding boxes using a json with keyword "subject_id", "category", "caption", and "bounding box".'
-prompt7 = 'What are the coordinates of the clock in the image?'
+prompt6 = 'Please caption the notable attributes in the provided image. List and describe all marked subjects in the image with their categories, detailed captions, and bounding boxes using json with keyword "subject_id", "category", "caption", and "bounding box".'
+prompt7 = 'What are the coordinates of the persons elbow in the image?'
 prompt8 = 'What are the coordinates of the picture of the childs face in the image?'
 
 messages = [
@@ -41,7 +41,7 @@ messages = [
             },
             {
                 "type": "text",
-                "text": f"{prompt8}"
+                "text": f"{prompt7}"
             }
         ]
     }
@@ -49,7 +49,8 @@ messages = [
 
 start = time.time()
 response = client.chat.completions.create(
-    model="Nvidia/Cosmos-Reason2-2B",
+    #model="Nvidia/Cosmos-Reason2-2B",
+    model="cyankiwi/gemma-4-26B-A4B-it-AWQ-4bit",
     messages=messages,
     max_tokens=2048
 )
